210e9a8ac436760031bc72d70839572f7fc37f45
yste <song.of.yste@gmail.com>
2013-06-16 14:04:10 +0900

    H1タグのテスト・実装が一旦完了.

--- src/main/java/tddbc/WikiEngine.java
+++ src/main/java/tddbc/WikiEngine.java
@@ -1,10 +1,18 @@
 package tddbc;
+import java.util.regex.*;
+import java.util.*;
 
 public class WikiEngine {
 
 	public String getText(String text) {
-		// TODO Auto-generated method stub
-		return text;
+		String str = text;
+		String regex = "^=[\t ]*(.+[^\t ]+)[\t ]*=$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher m = pattern.matcher(text);
+		if(m.find()) {
+			str = "<H1>" + m.group(1) + "</H1>";
+		}
+		return str;
 	}
 
 }
--- src/test/java/tddbc/WikiEngineTest.java
+++ src/test/java/tddbc/WikiEngineTest.java
@@ -4,14 +4,32 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class WikiEngineTest {
-
-	@Test
-	public void test記法関係なしに文字を返す() {
-		WikiEngine sut = new WikiEngine();
-		assertThat(sut.getText("text"), is("text"));
+	WikiEngine sut;
+	
+	@Before
+	public void setUp() {
+		sut = new WikiEngine();
 	}
 
+	
+	@Test
+	public void test記法関係なしに文字を返す() {
+		assertThat(sut.getText("text"), is("text"));
+	}
+	
+	@Test
+	public void testイコールで括られた文字列にH1タグで括られた文字列を返す() {
+		assertThat(sut.getText("= TDDBC ="), is("<H1>TDDBC</H1>"));
+		assertThat(sut.getText("=TDDBC="), is("<H1>TDDBC</H1>"));
+		assertThat(sut.getText("=   TDDBC   ="), is("<H1>TDDBC</H1>"));
+		assertThat(sut.getText("= TDD BC ="), is("<H1>TDD BC</H1>"));
+		assertThat(sut.getText("= TDD = BC ="), is("<H1>TDD = BC</H1>"));
+		assertThat(sut.getText("=\tTDD = BC ="), is("<H1>TDD = BC</H1>"));
+	}
+	
+	
 }
